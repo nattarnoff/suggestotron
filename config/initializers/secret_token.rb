f72e52d8007593b453ce37b89db28ef3a3d3402e
Greg Tarnoff <greg.tarnoff@gmail.com>
2013-02-10 14:16:37 -0600

    Add ALL THE THINGS! Ruby201/Railsbridge suggestotron

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = 'f1d6cdee6b50c61095b3872c783b696b971ac9bc46a349d3bf61279d673ce44d1e48d9bb77520228dac10b786c3b17ac6171dfdff056cf249eadae17b223d152'
